2978ba7cbd9412dbfdcaf5a04ad75f72edd00245
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-01 00:00:27 -0300

    get metod update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,8 +13,24 @@ export class TasksService {
     private tasksRepository: TasksRepository,
   ) {}
 
-  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
-    //
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+
+    const query = this.tasksRepository.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
   }
 
   async getTaskById(id: string): Promise<Task> {
