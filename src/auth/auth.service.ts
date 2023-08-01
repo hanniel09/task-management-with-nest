5cc6e7bd812ea88b653a20835afbc9f29d28aea8
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-01 14:30:05 -0300

    post metods create

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,14 @@ export class AuthService {
     @InjectRepository(User)
     private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.createUser(authCredentialsDto);
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+  }
 }
