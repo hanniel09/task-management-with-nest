042f638aa11ccb8f87956018c77215e22557de9b
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-08-03 18:45:45 -0300

    Cross-env

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 
+console.log(process.env.NODE);
+console.log(process.env.SOMETHING);
+
 async function bootstrap() {
+  /*
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -11,5 +15,6 @@ async function bootstrap() {
   const port = 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
+  */
 }
 bootstrap();
